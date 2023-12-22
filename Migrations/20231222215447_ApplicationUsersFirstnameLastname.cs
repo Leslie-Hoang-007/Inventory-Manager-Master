ebb497538cf6d55610e6de8efdb15b4ebe7a63ca
Leslie-Hoang-007 <111301770+Leslie-Hoang-007@users.noreply.github.com>
2023-12-22 17:07:31 -0500

    add Application User FirstName and LastName Fields

---
+++ Migrations/20231222215447_ApplicationUsersFirstnameLastname.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventory_Manager.Migrations
+{
+    public partial class ApplicationUsersFirstnameLastname : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Date", "Status" },
+                values: new object[] { new DateTime(2023, 7, 25, 21, 54, 47, 196, DateTimeKind.Utc).AddTicks(6617), "Arrived" });
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2023, 9, 13, 21, 54, 47, 196, DateTimeKind.Utc).AddTicks(6615));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2023, 11, 2, 21, 54, 47, 196, DateTimeKind.Utc).AddTicks(6601));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2023, 7, 25, 21, 54, 47, 196, DateTimeKind.Utc).AddTicks(6722));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2023, 9, 13, 21, 54, 47, 196, DateTimeKind.Utc).AddTicks(6721));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2023, 11, 2, 21, 54, 47, 196, DateTimeKind.Utc).AddTicks(6719));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Date", "Status" },
+                values: new object[] { new DateTime(2023, 7, 25, 21, 45, 3, 759, DateTimeKind.Utc).AddTicks(1485), "OutForDelivery" });
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2023, 9, 13, 21, 45, 3, 759, DateTimeKind.Utc).AddTicks(1484));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2023, 11, 2, 21, 45, 3, 759, DateTimeKind.Utc).AddTicks(1475));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2023, 7, 25, 21, 45, 3, 759, DateTimeKind.Utc).AddTicks(1596));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2023, 9, 13, 21, 45, 3, 759, DateTimeKind.Utc).AddTicks(1595));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2023, 11, 2, 21, 45, 3, 759, DateTimeKind.Utc).AddTicks(1593));
+        }
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Inventory_Manager.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+    }
+}
