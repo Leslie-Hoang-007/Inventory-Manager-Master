e6f7033ecd1c5f6d6b69ad1b777057ba96e2d71b
Leslie-Hoang-007 <111301770+Leslie-Hoang-007@users.noreply.github.com>
2023-12-22 18:02:41 -0500

    removed create action from products controller

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -70,27 +70,6 @@ namespace Inventory_Manager.Controllers
         }
    
 
-        // GET: Products/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Products/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Date,Name,Quantity,Paid,Status,TotalPrice")] Products products)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(products);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(products);
-        }
 
         [Authorize]
         // GET: Products/Edit/5
